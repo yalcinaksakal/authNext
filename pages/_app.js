d74d3578585c51b10c7419b63dc1eb4c52e27ff5
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-09 03:32:33 +0300

    authNext

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,26 @@ import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import { Provider } from "react-redux";
 import store from "../store";
-
+import Spinner from "../components/Layout/Spinner";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 function AuthApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const start = () => setIsLoading(true);
+    const end = () => setIsLoading(false);
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeComplete", end);
+    router.events.on("routeChangeError", end);
+
+    return () => {
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", end);
+      router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <Head>
@@ -18,9 +36,8 @@ function AuthApp({ Component, pageProps }) {
           content="Basic authentication example using nextJS"
         />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+
+      <Layout>{isLoading ? <Spinner /> : <Component {...pageProps} />}</Layout>
     </Provider>
   );
 }
